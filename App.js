d7e91aed8ad88fbda0e07330f7d51fcf36a10663
deeprupani <75526959+deeprupani@users.noreply.github.com>
2021-09-16 20:03:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createAppContainer } from "react-navigation";
+import TransactionScreen from "./screens/BookTransactionScreen";
+import SearchScreen from './screens/SearchScreen';
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer/>
+  } 
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: { screen: TransactionScreen },
+  Search: { screen: SearchScreen }
+})
+
+const AppContainer = createAppContainer(TabNavigator)
+const styles = StyleSheet.create({
+
+});
